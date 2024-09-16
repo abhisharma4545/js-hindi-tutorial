88974c85ae9b7a0e41be39e574dbdfc30f42e964
Abhishek Kumar <sharmaabhisheksharma178@gmail.com>
2024-09-16 13:07:49 +0530

    keyboard

--- Asyn project/Abhi.js
+++ Asyn project/Abhi.js
@@ -1,28 +1,28 @@
-// generate a rondom color
-
-const randomColor = function(){
+const randomColor = function() {
     const hex = "0123456789ABCDEF";
-    let color = "#"
-    for(let i = 0; i< 6; i++){
-        color += hex[Math.floor((Math.random() *16))];
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
-}
-let intervalId
-const startChangingColor = function(){
- if(intervalId){
-    intervalId = setInterval(changeBgColor,1000);
-    
- }
- function changeBgColor (){
+};
+
+let intervalId = null;
+
+const startChangingColor = function() {
+    if (!intervalId) { // Check if intervalId is null to prevent multiple intervals
+        intervalId = setInterval(changeBgColor, 1000);
+    }
+};
+
+function changeBgColor() {
     document.body.style.backgroundColor = randomColor();
- }
 }
-const stopChangingColor = function(){
+
+const stopChangingColor = function() {
     clearInterval(intervalId);
     intervalId = null;
-}
+};
 
-document.querySelector('#start').addEventListener('click', startChangingColor)
-
-document.querySelector('#stop').addEventListener('click', stopChangingColor)
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
